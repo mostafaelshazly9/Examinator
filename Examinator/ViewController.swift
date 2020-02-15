4b64e11a227643b0ee7add2e859b8ebcceefa42e
mostafaelshazly9 <mostafa.elshazly.000@gmail.com>
2020-02-15 18:23:13 +0200

    Initial Commit

---
+++ Examinator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Examinator
+//
+//  Created by Mostafa Elshazly on 2/15/20.
+//  Copyright © 2020 Mostafa Elshazly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
